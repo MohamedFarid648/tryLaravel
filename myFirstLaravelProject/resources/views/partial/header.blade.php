2c6d828dea4b6260ed44fb3aa2eee0a92c310788
mohamed <mohamed9abdullah@gmail.com>
2018-10-01 17:46:04 +0200

    create layout,views

---
+++ myFirstLaravelProject/resources/views/partial/header.blade.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-md navbar-light">
+		<button class="navbar-toggler" data-toggle="collapse" data-target="#n1"><span class="navbar-toggler-icon"></span></button>
+
+		<div class="collapse navbar-collapse" id="n1">
+			<ul class="navbar-nav">
+				<li class="nav-item">
+					<a href="#" class="nav-link">home</a>
+				</li>
+				<li class="nav-item">
+					<a href="#" class="nav-link">page1</a>
+				</li>
+				<li class="nav-item">
+					<a href="#" class="nav-link">page2</a>
+				</li>
+			</ul>
+
+
+
+		</div>
+
+
+	</nav>
